7bb630430073caa16e92a934861ed00f36293a86
Mihir Patel <mihir.fluxbyte@gmail.com>
2022-10-27 15:43:53 +0530

    change console

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.info("[Nest]        - ___________ __________     Starting application on :  http://localhost:3000")
   await app.listen(3000);
 }
 bootstrap();
