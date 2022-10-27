f1c1cad386f77eca75900b94a988eae0472061d9
Mihir Patel <mihir.fluxbyte@gmail.com>
2022-10-27 16:17:12 +0530

    global logging interceptor

---
+++ src/Interceptors/LoggingInterceptor.ts
@@ -0,0 +1,14 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from "@nestjs/common";
+import { Observable, tap } from "rxjs";
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const now = Date.now();
+    return next
+      .handle()
+      .pipe(
+        tap(() => console.log(`\nRequested Url: ${context.getArgs()[0].originalUrl}\n\t Time: ${Date.now() - now}ms.\n`)),
+      );
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggingInterceptor } from './Interceptors/LoggingInterceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new LoggingInterceptor());
   console.info("[Nest]        - ___________ __________     Starting application on :  http://localhost:3000")
   await app.listen(3000);
 }
